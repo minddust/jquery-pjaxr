85f847a0974219ec60037d38e0d186d3f0338a15
Stephan Groß <stephan@minddust.com>
2013-08-07 12:41:36 +0200

    Add pjax delivery support

--- tests/app.py
+++ tests/app.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 from werkzeug.routing import BaseConverter
 
 
@@ -11,6 +11,30 @@ class RegexConverter(BaseConverter):
     def __init__(self, url_map, *items):
         super(RegexConverter, self).__init__(url_map)
         self.regex = items[0]
+
+
+# thanks @zachwill - https://github.com/zachwill/pjax_flask/blob/master/app.py
+def pjax(template):
+    if 'X-PJAX' in request.headers:
+        return render_template(_pjaxify_template_var(template))
+    return render_template(template)
+
+
+# thanks @jacobian - https://github.com/jacobian/django-pjax/blob/master/djpjax.py
+def _pjaxify_template_var(template_var):
+    if isinstance(template_var, (list, tuple)):
+        template_var = type(template_var)(_pjaxify_template_name(name) for name in template_var)
+    elif isinstance(template_var, basestring):
+        template_var = _pjaxify_template_name(template_var)
+    return template_var
+
+
+def _pjaxify_template_name(name):
+    if '.' in name:
+        name = '%s-pjax.%s' % tuple(name.rsplit('.', 1))
+    else:
+        name += '-pjax'
+    return name
 
 
 @app.route('/')
@@ -25,7 +49,7 @@ def pjaxr():
 
 @app.route('/<regex("[\w]+\.html"):view>/')
 def view(view):
-    return render_template('views/{view}'.format(view=view))
+    return pjax('views/{view}'.format(view=view))
 
 
 @app.route('/<regex("/libs/[\w]+\.[js|css]"):lib>/')
