eddc05da0ef955d688dfd9220b6b7f80e4a045e6
Stephan Groß <stephan@minddust.com>
2013-08-07 10:56:52 +0200

    Add initial flask webserver

---
+++ tests/app.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
