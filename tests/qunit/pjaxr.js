6790be2a3567ee6e304375b09e5720e77d331728
Stephan Groß <stephan@minddust.com>
2013-08-08 13:52:55 +0200

    Fix missing back applyment

--- tests/qunit/pjaxr.js
+++ tests/qunit/pjaxr.js
@@ -72,14 +72,42 @@ asyncTest('push 2nd level url', function() {
         equal(window.location.pathname, '/home/');
         equal($('head > title').html(), 'home');
         equal($('head > meta').length, 3);
+        equal($('#menu-extra-entry').html().trim(), '');
+        equal($('#content').html().trim(), '<h1>home - pjax</h1>');
 
         $(document).one('pjaxr:end', function() {
             equal(window.location.pathname, '/blog/');
             equal($('head > title').html(), 'blog');
+            equal($('head > meta').length, 2);
+            $.each($('head > meta'), function(index, value) {
+                var $meta = $(value);
+                if ($meta.attr('name') === 'author') { equal($meta.attr('content'), 'stephan'); }
+                else if ($meta.attr('name') === 'title') { equal($meta.attr('content'), 'blog'); }
+                else { ok(false); }
+            });
+            equal($('#menu-extra-entry').html().trim(), '<a href="/entry-x/">click here for the latest blog entry</a>');
+            equal($('#content').html().trim(), '<h1>blog list - pjax</h1><br><h2>Entry X</h2><a href="/entry-x/">click here to read more</a>');
 
-            history.go(-(history.length - 1));
+            history.back();
 
-            start();
+            setTimeout(function() {
+                equal(window.location.pathname, '/home/');
+                equal($('head > title').html(), 'home');
+                equal($('head > meta').length, 3);
+                $.each($('head > meta'), function(index, value) {
+                    var $meta = $(value);
+                    if ($meta.attr('name') === 'author') { equal($meta.attr('content'), 'minddust'); }
+                    else if ($meta.attr('name') === 'title') { equal($meta.attr('content'), 'home'); }
+                    else if ($meta.attr('name') === 'description') { equal($meta.attr('content'), 'home of everything'); }
+                    else { ok(false); }
+                });
+                equal($('#menu-extra-entry').html().trim(), '');
+                equal($('#content').html().trim(), '<h1>home - pjax</h1>');
+
+                history.go(-(history.length - 1));
+
+                start();
+            }, 0);
         });
 
         $('a[href="/blog/"]').trigger('click');
