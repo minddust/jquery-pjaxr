50a68c820e4b3704310599360a2b63d8a8914364
Stephan Groß <stephan@minddust.com>
2014-03-14 12:59:58 +0100

    Add favicon to catch travis 404's

--- test_app/urls.py
+++ test_app/urls.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import patterns, url
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 
 urlpatterns = patterns('',
@@ -9,5 +9,7 @@ urlpatterns = patterns('',
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^project/$', TemplateView.as_view(template_name='project.html'), name='project'),
     url(r'^project/blog/$', TemplateView.as_view(template_name='project_blog.html'), name='project_blog'),
-    url(r'^project/gallery/$', TemplateView.as_view(template_name='project_gallery.html'), name='project_gallery')
+    url(r'^project/gallery/$', TemplateView.as_view(template_name='project_gallery.html'), name='project_gallery'),
+
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),  # prevent travis 404 logs
 )
