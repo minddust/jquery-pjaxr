64590b91cef3e2f4c599a60e797ab37ea6ad6f42
Stephan Groß <stephan@minddust.com>
2014-03-16 20:02:04 +0100

    Fix #12

--- test_app/urls.py
+++ test_app/urls.py
@@ -7,7 +7,7 @@ from django.views.generic import TemplateView, RedirectView
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
-    url(r'^invalid-metatag/$', TemplateView.as_view(template_name='invalid_metatag.html'), name='invalid_metatag'),
+    url(r'^ignored-metatag/$', TemplateView.as_view(template_name='ignored_metatag.html'), name='ignored_metatag'),
     url(r'^project/$', TemplateView.as_view(template_name='project.html'), name='project'),
     url(r'^project/blog/$', TemplateView.as_view(template_name='project_blog.html'), name='project_blog'),
     url(r'^project/gallery/$', TemplateView.as_view(template_name='project_gallery.html'), name='project_gallery'),
