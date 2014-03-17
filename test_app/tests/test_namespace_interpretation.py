65710cf537287772fcecad0e5e6144044056b425
Stephan Groß <stephan@minddust.com>
2014-03-17 12:55:56 +0100

    Fix testcases

--- test_app/tests/test_namespace_interpretation.py
+++ test_app/tests/test_namespace_interpretation.py
@@ -12,7 +12,7 @@ class NamespaceInterpretationTest(SeleniumTestCase):
     def test_namespaces_after_pjaxr(self):
         self.assertTitle('index-title')
         self.assertContent('index-content')
-        self.assertBodyAttr('data-pjaxr-namespace', 'md.index')
+        self.assertBodyNamespace('md.index')
 
         about_link = self.browser.find_element_by_css_selector('#about-link')
         about_link.click()
