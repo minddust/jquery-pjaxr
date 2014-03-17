aabb109197350d52e0e3c60846ae5341400bcd98
Jonas <jbraun@uni-koblenz.de>
2014-03-17 12:30:06 +0100

    adding new test case which shows that chrome/webkit fails on double-pjaxr, no-pjaxr and back

--- test_app/tests/test_pjaxr_request.py
+++ test_app/tests/test_pjaxr_request.py
@@ -44,3 +44,39 @@ class PjaxrRequestTest(SeleniumTestCase):
         self.assertBodyAttr('js-pjaxr-fail', None)
         self.assertBodyAttr('js-pjaxr-always', None)
         self.assertBodyAttr('js-pjaxr-end', None)
+
+    def test_pjaxr_request_depth_2_no_pjaxr_and_back(self):
+        self.assertTitle('index-title')
+        self.assertContent('index-content')
+        self.assertBodyAttr('js-pjaxr-done', None)
+
+        about_link = self.browser.find_element_by_css_selector('#about-link')
+        about_link.click()
+
+        self.wait.until(lambda browser: browser.title == 'about-title')
+        self.assertTitle('about-title')
+        self.assertContent('about-content')
+
+        project_link = self.browser.find_element_by_css_selector('#project-link')
+        project_link.click()
+
+        self.wait.until(lambda browser: browser.title == 'project-title')
+        self.assertTitle('project-title')
+        self.assertContent('project-content')
+
+        no_pjaxr_response_link = self.browser.find_element_by_css_selector('#no-pjaxr-response-link')
+        no_pjaxr_response_link.click()
+
+        self.wait.until(lambda browser: browser.title == 'no-pjaxr-response-title')
+        self.assertTitle('no-pjaxr-response-title')
+        self.assertContent('no-pjaxr-response-content')
+
+        self.browser.back()
+
+        # On Chrome/Webkit caching will return the content out of the pjaxr xhr-response, not a new initial content.
+        # should be fixed in #16
+        self.wait.until(lambda browser: browser.find_element_by_css_selector('#site'))
+
+        self.wait.until(lambda browser: browser.title == 'project-title')
+        self.assertTitle('project-title')
+        self.assertContent('project-content')
